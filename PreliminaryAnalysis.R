b3669676dae915ef1009edd52945ef7814413f94
Wolfffff <swwolf@princeton.edu>
2020-01-22 09:35:14 +0300

    Preliminary script

---
+++ PreliminaryAnalysis.R
@@ -0,0 +1,10 @@
+### EEB521 Termite Project Preliminary Analysis
+
+
+
+# 22012020 ----------------------------------------------------------------
+
+if(!require(install.load)){
+  install.packages("install.load")
+  library(install.load)
+}
