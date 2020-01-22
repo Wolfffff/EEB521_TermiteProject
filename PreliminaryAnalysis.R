1c6cd6a0457492de0eb62064bfed960dcc8f72cc
Wolfffff <scott.w.wolf1@gmail.com>
2020-01-22 10:53:50 +0300

    Fixing initial data -- loading into R for the first time

--- PreliminaryAnalysis.R
+++ PreliminaryAnalysis.R
@@ -1,10 +1,26 @@
 ### EEB521 Termite Project Preliminary Analysis
 
 
-
 # 22012020 ----------------------------------------------------------------
 
 if(!require(install.load)){
   install.packages("install.load")
   library(install.load)
 }
+
+# WD to context
+# https://stackoverflow.com/questions/13672720/r-command-for-setting-working-directory-to-source-file-location-in-rstudio
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+install_load("tidyverse",
+             "ggplot",
+             "dplyr")
+
+diversity <- read_csv("data/diversity/DiversityBiomass.csv")
+leafSeedRootShoot <- read_csv("data/diversity/LeafSeedRootShoot.csv")
+
+g1 <- ggplot(diversity, aes(x=Alive, y=Weight)) +
+  stat_boxplot(fill = NA)
+g1
+
+
